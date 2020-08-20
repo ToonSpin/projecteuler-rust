2b7915426236e93d4dc83e1ca7c3566b52f2e613
Toon Spin <toon.spin@gmail.com>
2020-08-20 21:45:08 +0200

    Improve problem 3

--- src/bin/problem003.rs
+++ src/bin/problem003.rs
@@ -3,32 +3,29 @@
 const INPUT_NUMBER: u64 = 600851475143;
 
 fn main() {
-    let upper_bound = (INPUT_NUMBER as f64).sqrt().floor() as u64;
-
     let mut primes: Vec<u64> = vec![2];
     let mut next_prime = 3;
     let mut max_prime_factor = 0;
+    let mut n = INPUT_NUMBER;
 
-    let progress_bar = indicatif::ProgressBar::new(upper_bound);
+    while n > 1 {
+        let mut not_prime = false;
 
-    while next_prime <= upper_bound {
-        let mut factor_found = false;
         for p in primes.iter() {
             if next_prime % *p == 0 {
-                factor_found = true;
+                not_prime = true;
                 break;
             }
         }
-        if !factor_found {
+        if !not_prime {
             primes.push(next_prime);
-            if INPUT_NUMBER % next_prime == 0 {
+            if n % next_prime == 0 {
                 max_prime_factor = next_prime;
+                n /= max_prime_factor;
             }
         }
         next_prime += 2;
-        progress_bar.inc(2);
     }
-    progress_bar.finish();
 
     println!("The largest prime factor is {}", max_prime_factor);
 }
