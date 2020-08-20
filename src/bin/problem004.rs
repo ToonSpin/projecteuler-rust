2ad6fa3df1b88859bd9ae84aa0929c5848f09856
Toon Spin <toon.spin@gmail.com>
2020-08-20 22:03:52 +0200

    Problem 4

---
+++ src/bin/problem004.rs
@@ -0,0 +1,49 @@
+// A palindromic number reads the same both ways. The largest palindrome made
+// from the product of two 2-digit numbers is 9009 = 91 × 99.
+//
+// Find the largest palindrome made from the product of two 3-digit numbers.
+
+fn is_palindromic(n: u32) -> bool {
+    let digits = get_digits(n);
+    let max = digits.len() / 2 + 1;
+    for i in 0..max {
+        if digits[i] != digits[digits.len() - 1 - i] {
+            return false;
+        }
+    }
+    true
+}
+
+fn is_product_of_3d_pair(n: u32) -> bool {
+    let upper_bound = (n as f32).sqrt().ceil() as u32;
+    let mut factor = 100;
+    while factor <= upper_bound {
+        if n % factor == 0 {
+            if n / factor < 1000 && n / factor >= 100 {
+                return true;
+            }
+        }
+        factor += 1;
+    }
+    false
+}
+
+fn get_digits(mut n: u32) -> Vec<u32> {
+    let mut digits = Vec::new();
+    while n > 0 {
+        digits.push(n % 10);
+        n /= 10;
+    }
+    digits
+}
+
+fn main() {
+    let mut n = 998001;
+    loop {
+        if is_palindromic(n) && is_product_of_3d_pair(n) {
+            break;
+        }
+        n -= 1;
+    }
+    println!("The largest palindrome is {}", n);
+}
